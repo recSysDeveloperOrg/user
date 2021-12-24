f842539dd6e5d70038af601b406bd6b8cd218e84
lijunyu <lijunyu@ljygogogo.com>
2021-12-25 00:49:10 +0800

    grpc server start

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
 	"user/config"
 	"user/cred"
+	"user/idl/gen/user"
 	"user/model"
 )
 
@@ -14,6 +19,22 @@ func main() {
 		panic(err)
 	}
 	if err := cred.InitJwt(); err != nil {
+		panic(err)
+	}
+
+	var port int
+	var ip string
+	flag.IntVar(&port, "port", 50002, "port for the service")
+	flag.StringVar(&ip, "ip", "127.0.0.1", "ip for the service")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", ip, port))
+	if err != nil {
+		panic(err)
+	}
+	grpcServer := grpc.NewServer()
+	user.RegisterUserServiceServer(grpcServer, &Handler{})
+	if err := grpcServer.Serve(lis); err != nil {
 		panic(err)
 	}
 }
